5f373808fc7c137692d0a35e096b4d56a5f933bd
thou42 <thou@student.42.fr>
2017-06-27 17:58:22 +0200

    Mar 27 jui 2017 17:58:22 CEST

--- filler_test.sh
+++ filler_test.sh
@@ -5,11 +5,35 @@ make -C /tmp/filler_tester
 git clone https://github.com/houtl/filler.git /tmp/filler_resources
 cp -R /tmp/filler_resources/resources .
 cp /tmp/filler_tester/filler_tester ./resources
-PLAYER=$(cat auteur)
-if [ -e "$PLAYER.filler" ]; then
-	cp $PLAYER.filler ./resources/players
+red="\033[0;31m"
+green="\033[0;32m"
+blue="\033[0;34m"
+nocolor="\033[0m"
+if [ -e "auteur" ]; then
+	PLAYER=$(cat auteur)
+elif [ -e "author" ]; then
+	PLAYER=$(cat author)
+else
+	echo $red
+	echo "Ne peut pas trouver$blue FICHIER AUTEUR$red, verifier bien que vous etes bien dans la racine de depot"
+	echo $nocolor
+	exit
 fi
-./resources/filler_tester << EOF
+if [ -e "$PLAYER.filler" ]; then
+	cp "$PLAYER.filler" ./resources/players
+else
+	make re 1>/dev/null 2>&1
+	if [ -e "$PLAYER.filler" ]; then
+		cp "$PLAYER.filler" ./resources/players
+	else
+		echo $red
+		echo "Ne peut pas trouver$blue $PLAYER.filler$red, verifier que vous etes bien dans la racine de depot"
+		echo $nocolor
+		exit
+	fi
+fi
+cd resources
+./filler_tester << EOF
 $PLAYER
 y
 5
